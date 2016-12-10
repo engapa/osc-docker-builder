f0381c33c55703ed38c66e013b81670aab1fbd11
Enrique Garcia <engapa@gmail.com>
2016-12-10 20:54:36 +0100

    Improve tests

--- osc_docker_builder/tests/osc.py
+++ osc_docker_builder/tests/osc.py
@@ -53,7 +53,7 @@ class OSCTestCase(unittest.TestCase):
         """
         build_path = 'test_build'
         os.makedirs(build_path)
-        with open(build_path + '/test', 'wr') as test_file:
+        with open(build_path + '/test', 'wb') as test_file:
             test_file.write('test')
         osc.clean_build_dir(build_path, remove=True, create=False)
         self.assertFalse(os.path.exists(build_path + '/test'))
@@ -128,7 +128,9 @@ class OSCTestCase(unittest.TestCase):
             raise Exception('Test build path not created')
 
     def test_client_config(self):
-
+        """
+        Tests client config
+        """
         client_name = 'myclient'
         release = 'release'
 
